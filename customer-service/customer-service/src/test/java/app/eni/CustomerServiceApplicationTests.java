db3af868b794fd8c3aa78a39e0955a8e987c005a
KhaoulaJRIDI <34188950+KhaoulaJRIDI@users.noreply.github.com>
2022-10-14 08:20:41 +0100

    Add files via upload

---
+++ customer-service/customer-service/src/test/java/app/eni/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.eni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
